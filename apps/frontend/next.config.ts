86a4ad8c4275eaa2935cdb7757db3fd30c7db44c
Nguyễn Lương Minh Thông <nguyenluongminhthong@gmail.com>
2025-09-30 09:19:33 +0700

    fix(frontend): adjust next config for CI build

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  experimental: {
+    esmExternals: "loose",
+  },
+  transpilePackages: [
+    "@rainbow-me/rainbowkit",
+    "@vanilla-extract/sprinkles",
+  ],
 };
 
 export default nextConfig;
